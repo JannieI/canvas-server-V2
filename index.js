090a256111e6f8395802009312610c3db2e4f0a3
JannieI <jimmelman@gmail.com>
2018-02-08 18:40:04 +0200

    Add POST method, v1

--- index.js
+++ index.js
@@ -20,18 +20,23 @@ var upload = multer();
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.post('/api/sayhello', upload.array(), function (request, response) {
+app.post('/api', upload.array(), function (request, response) {
     var name = request.body.name;
+    console.log('/api/sayhello start', request.url, request.method, request.body, upload);
     if (!isNaN(name)) {
         response
             .status(400)
             .send('No string as name');
     }
     else {
-        console.log('Hello ' + name);
+        request.on('data', function (data) {
+            data = data.toString();
+            console.log('Hello ' + data);
+        });
     }
     response.send('POST request to homepage');
 });
+// http://example.com/page?parameter=value&also=another
 app.get('/', function (request, response) {
     response.send('Hello World ... +');
 });
@@ -44,39 +49,50 @@ app.get('/api/sayhello/:name', function (request, response) {
             .send('No string as name');
     }
     else {
-        console.log('start SQlite');
+        console.log('start SQlite: use start, end or a person-name', name);
         // SQlite
         var values_1 = [];
         db.serialize(function () {
-            db.run("CREATE TABLE Test (col1, col2, col3)");
-            // let data = ['Ansi C', 'C'];
-            // let sql = `UPDATE langs
-            // SET name = ?
-            // WHERE name = ?`;
-            // db.run(sql, data, function(err) {
-            // db.run(`DELETE FROM langs WHERE rowid=?`, id, function(err) {
-            db.run("INSERT INTO Test VALUES (?, ?, ?)", ['a1', 'b1', 'c1']);
-            db.run("INSERT INTO Test VALUES (?, ?, ?)", ['a2', 'b2', 'c2']);
-            db.run("INSERT INTO Test VALUES (?, ?, ?)", ['a3', 'b3', 'c3']);
-            // each, get (1st)
-            db.all("SELECT * FROM Test", function (err, data) {
-                values_1.push(data);
-                console.log('after get SQlite', data, values_1);
-                response.json({ data: data });
-            });
-            console.log('after loop SQlite', values_1);
-        });
-        // db.close();
-        db.close(function (err) {
-            if (err) {
-                return console.error(err.message);
+            if (name == ':start') {
+                db.run("CREATE TABLE Test (col1, col2, col3)");
+                // let data = ['Ansi C', 'C'];
+                // let sql = `UPDATE langs
+                // SET name = ?
+                // WHERE name = ?`;
+                // db.run(sql, data, function(err) {
+                // db.run(`DELETE FROM langs WHERE rowid=?`, id, function(err) {
+                db.run("INSERT INTO Test VALUES (?, ?, ?)", ['a1', 'b1', 'c1']);
+                db.run("INSERT INTO Test VALUES (?, ?, ?)", ['a2', 'b2', 'c2']);
+                db.run("INSERT INTO Test VALUES (?, ?, ?)", ['a3', 'b3', 'c3']);
+                console.log('created new SQlite Table');
+                response.json('created new SQlite Table');
             }
-            console.log('Closed the database connection.');
+            else {
+                // Close SQlite DB
+                if (name == ':end') {
+                    db.close(function (err) {
+                        if (err) {
+                            return console.error(err.message);
+                        }
+                        console.log('Closed the database connection.');
+                    });
+                }
+                else {
+                    // each, get (1st)
+                    db.all("SELECT * FROM Test", function (err, data) {
+                        if (err) {
+                            console.log('use name start first, and end ...');
+                        }
+                        values_1.push(data);
+                        console.log('after get SQlite', data, values_1);
+                        response.json({ data: data });
+                    });
+                }
+            }
+            // response.json({
+            //     'message': name
+            // });          
         });
-        console.log('after SQlite');
-        // response.json({
-        //     'message': name
-        // });
     }
 });
 // http:localhost:3000/api/sayhello?name=NodeJS
@@ -100,4 +116,10 @@ app.get('/dashboards', function (request, response) {
     response.json(data);
 });
 app.listen(3000);
+// curl -i -X GET http://rest-api.io/items
+// curl -i -X GET http://rest-api.io/items/5069b47aa892630aae059584
+// curl -i -X DELETE http://rest-api.io/items/5069b47aa892630aae059584
+// curl -i -X POST -H 'Content-Type: application/json' -d '{"name": "New item", "year": "2009"}' http://rest-api.io/items
+// curl -i -X PUT -H 'Content-Type: application/json' -d '{"name": "Updated item", "year": "2010"}' http://rest-api.io/items/5069b47aa892630aae059584
+// 
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -23,20 +23,25 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.post('/api/sayhello', upload.array(), (request, response) => {
+app.post('/api', upload.array(), (request, response) => {
     var name = request.body.name;
-
+    console.log('/api/sayhello start', request.url, request.method, request.body, upload)
     if (!isNaN(name)) {
         response
             .status(400)
             .send('No string as name');
     } else {
-        console.log('Hello ' + name);
+        request.on('data', function(data) {
+            data = data.toString();
+
+            console.log('Hello ' + data);
+        })
     }
 
     response.send('POST request to homepage');
 });
 
+// http://example.com/page?parameter=value&also=another
 app.get('/', function (request, response) {
     response.send('Hello World ... +');
 });
@@ -51,45 +56,52 @@ app.get('/api/sayhello/:name', (request, response) => {
             .send('No string as name');
     } else {
 
-        console.log('start SQlite');        
+        console.log('start SQlite: use start, end or a person-name', name);        
         
         // SQlite
         let values: any[] = [];
         db.serialize(function () {
-            db.run("CREATE TABLE Test (col1, col2, col3)");
-          
-            // let data = ['Ansi C', 'C'];
-            // let sql = `UPDATE langs
-            // SET name = ?
-            // WHERE name = ?`;
-            // db.run(sql, data, function(err) {
 
-            // db.run(`DELETE FROM langs WHERE rowid=?`, id, function(err) {
-            db.run("INSERT INTO Test VALUES (?, ?, ?)", ['a1', 'b1', 'c1']);
-            db.run("INSERT INTO Test VALUES (?, ?, ?)", ['a2', 'b2', 'c2']);
-            db.run("INSERT INTO Test VALUES (?, ?, ?)", ['a3', 'b3', 'c3']);
-          
-            // each, get (1st)
-            db.all("SELECT * FROM Test", function (err, data) {
-                values.push(data)
-                console.log('after get SQlite',data, values);
-                response.json({data});
-            });
-            console.log('after loop SQlite', values);        
-        });
-        // db.close();
-        
-        db.close((err) => {
-          if (err) {
-            return console.error(err.message);
-          }
-          console.log('Closed the database connection.');
-        });
-        console.log('after SQlite');        
+            if (name == ':start') {
+                db.run("CREATE TABLE Test (col1, col2, col3)");
+            
+                // let data = ['Ansi C', 'C'];
+                // let sql = `UPDATE langs
+                // SET name = ?
+                // WHERE name = ?`;
+                // db.run(sql, data, function(err) {
 
+                // db.run(`DELETE FROM langs WHERE rowid=?`, id, function(err) {
+                db.run("INSERT INTO Test VALUES (?, ?, ?)", ['a1', 'b1', 'c1']);
+                db.run("INSERT INTO Test VALUES (?, ?, ?)", ['a2', 'b2', 'c2']);
+                db.run("INSERT INTO Test VALUES (?, ?, ?)", ['a3', 'b3', 'c3']);
+                console.log('created new SQlite Table')
+                response.json('created new SQlite Table')
+            } else {
+
+                // Close SQlite DB
+                if (name == ':end') {
+                    db.close((err) => {
+                    if (err) {
+                        return console.error(err.message);
+                    }
+                    console.log('Closed the database connection.');
+                    });
+                } else {
+                    // each, get (1st)
+                    db.all("SELECT * FROM Test", function (err, data) {
+                        if (err) {console.log('use name start first, and end ...')}
+                        values.push(data)
+                        console.log('after get SQlite',data, values);
+                        response.json({data});
+                    });
+                }
+            }
         // response.json({
         //     'message': name
-        // });
+        // });          
+        
+        });
     }
 });
 
@@ -117,3 +129,11 @@ app.get('/dashboards', function (request, response) {
 });
 
 app.listen(3000);
+
+// curl -i -X GET http://rest-api.io/items
+// curl -i -X GET http://rest-api.io/items/5069b47aa892630aae059584
+// curl -i -X DELETE http://rest-api.io/items/5069b47aa892630aae059584
+// curl -i -X POST -H 'Content-Type: application/json' -d '{"name": "New item", "year": "2009"}' http://rest-api.io/items
+// curl -i -X PUT -H 'Content-Type: application/json' -d '{"name": "Updated item", "year": "2010"}' http://rest-api.io/items/5069b47aa892630aae059584
+
+// 
