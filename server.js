c4db2d12208e512862c3f07bafd74504505050f9
JannieI <jimmelman@gmail.com>
2018-06-05 14:52:02 +0200

    Simple file upload

--- server.js
+++ server.js
@@ -101,9 +101,41 @@ eventEmitter.emit('connection');
 
 app.get('/index.htm', function (req, res) {
     res.sendFile( __dirname + "/" + "index.htm" );
- })
+})
+
+app.get('/process_get', function (req, res) {
+    // Prepare output in JSON format
+    response = {
+       first_name:req.query.first_name,
+       last_name:req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
+app.post('/file_upload', function (req, res) {
+    console.log(req.files.file.name);
+    console.log(req.files.file.path);
+    console.log(req.files.file.type);
+    var file = __dirname + "/" + req.files.file.name;
+    
+    fs.readFile( req.files.file.path, function (err, data) {
+       fs.writeFile(file, data, function (err) {
+          if( err ){
+             console.log( err );
+             }else{
+                response = {
+                   message:'File uploaded successfully',
+                   filename:req.files.file.name
+                };
+             }
+          console.log( response );
+          res.end( JSON.stringify( response ) );
+       });
+    });
+})
  
- app.post('/process_post', urlencodedParser, function (req, res) {
+app.post('/process_post', urlencodedParser, function (req, res) {
     // Prepare output in JSON format
     response = {
        first_name:req.body.first_name,
@@ -111,7 +143,7 @@ app.get('/index.htm', function (req, res) {
     };
     console.log(response);
     res.end(JSON.stringify(response));
- })
+})
  
  var server = app.listen(8080, function () {
     var host = server.address().address
