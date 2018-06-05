a8addde6e0194e43379daa8d77c91e36a6237b49
JannieI <jimmelman@gmail.com>
2018-06-05 13:51:50 +0200

    Server, v2

--- server.js
+++ server.js
@@ -7,30 +7,144 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-
+var fs = require("fs");
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var user = {
+    "user4" : {
+       "name" : "mohit",
+       "password" : "password4",
+       "profession" : "teacher",
+       "id": 4
+    }
+}
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+app.get('/listUsers', function (req, res) {
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        console.log( data );
+        res.end( data );
+    });
+})
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+app.get('/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       var users = JSON.parse( data );
+       var user = users["user" + req.params.id] 
+       console.log( user );
+       res.end( JSON.stringify(user));
+    });
+})
+
+app.post('/addUser', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        data["user4"] = user["user4"];
+        console.log( data );
+        res.end( JSON.stringify(data));
+    });
+})
+
+var id = 2;
+
+app.delete('/deleteUser', function (req, res) {
+
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       delete data["user" + 2];
+       
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+})
+
+
+
+// Events Example
+// Import events module
+var events = require('events');
+
+
+// Create an eventEmitter object
+var eventEmitter = new events.EventEmitter();
+
+// Create an event handler as follows
+var connectHandler = function connected() {
+   console.log('connection succesful.');
+  
+   // Fire the data_received event 
+   eventEmitter.emit('data_received');
+}
+
+// Bind the connection event with the handler
+eventEmitter.on('connection', connectHandler);
+ 
+// Bind the data_received event with the anonymous function
+eventEmitter.on('data_received', function(){
+   console.log('data received succesfully.');
 });
 
-// more routes for our API will happen here
+// Fire the connection event 
+eventEmitter.emit('connection');
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+console.log("Program Ended.");
 
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+var server = app.listen(8080, function () {
+ 
+    var host = server.address().address
+    var port = server.address().port
+    var p    = "users.json"
+
+    console.log("Example app listening at http://%s:%s")
+    console.log("  Host: Port", host, ":", port)
+    console.log("  Running: Folder: File", __dirname, __filename)
+    console.log("  os.hostname(), os.tmpdir(), os.release()", os.hostname(), os.tmpdir(), os.release())
+    console.log("  os.totalmem(), os.freemem()", os.totalmem(), os.freemem())
+    console.log("  path.dirname(p)", path.dirname(p))
+    console.log("  ", )
+    console.log("  ", )
+    
+})
+
+
+// Web Server Sample
+// var http = require('http');
+// var fs = require('fs');
+// var url = require('url');
+
+// // Create a server
+// http.createServer( function (request, response) {  
+//    // Parse the request containing file name
+//    var pathname = url.parse(request.url).pathname;
+   
+//    // Print the name of the file for which request is made.
+//    console.log("Request for " + pathname + " received.");
+   
+//    // Read the requested file content from file system
+//    fs.readFile(pathname.substr(1), function (err, data) {
+//       if (err) {
+//          console.log(err);
+//          // HTTP Status: 404 : NOT FOUND
+//          // Content Type: text/plain
+//          response.writeHead(404, {'Content-Type': 'text/html'});
+//       }else {	
+//          //Page found	  
+//          // HTTP Status: 200 : OK
+//          // Content Type: text/plain
+//          response.writeHead(200, {'Content-Type': 'text/html'});	
+         
+//          // Write the content of the file to response body
+//          response.write(data.toString());		
+//       }
+//       // Send the response body 
+//       response.end();
+//    });   
+// }).listen(8081);
+
+// // Console will print the message
+// console.log('Server running at http://127.0.0.1:8081/');
